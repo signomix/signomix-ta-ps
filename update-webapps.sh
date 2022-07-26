aeab882f8350442d01e199466bd2aef2e6d3426c
Grzegorz Skorupa <GSkorupa@nmg.pl>
2022-07-26 10:36:49 +0200

    wip

--- update-webapps.sh
+++ update-webapps.sh
@@ -2,6 +2,6 @@
 rm -r -f src/main/resources/META-INF/resources/*
 mkdir -p src/main/resources/META-INF/resources/blog
 cp -r src/main/webapp/home/public/* src/main/resources/META-INF/resources
-cp src/main/webapp/home/public/local_resources/* src/main/resources/META-INF/resources
+cp src/main/webapp/home/public/local_resources/* src/main/resources/META-INF/resources/resources
 cp -r src/main/webapp/blog/public/* src/main/resources/META-INF/resources/blog
-cp src/main/webapp/blog/public/local_resources/* src/main/resources/META-INF/resources/blo  g
+cp src/main/webapp/blog/public/local_resources/* src/main/resources/META-INF/resources/blog/resources
